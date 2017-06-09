c63c08efebb73ffefb62f2a8f18c52ae636de1bf
dflozanot <daigo_1001@hotmail.com>
2017-06-09 10:13:59 -0500

    Se cambia titulo
    Primer paso para empeza con el proyecto

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-<title>PS</title>
+<title>Periodismo</title>
 <meta charset="UTF-8">
 <meta name="viewport"
 	content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1">
